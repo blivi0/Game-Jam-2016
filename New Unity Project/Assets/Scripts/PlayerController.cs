3e2ab0b62c282a530d4f7579d23138f352083612
David K Amadeo <damadeo@UCSD.EDU>
2016-10-14 21:40:39 -0700

    Progress before leaving

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+
+    private Camera camera;
+
+    void Awake() {
+        camera = transform.FindChild("Camera").GetComponent<Camera>();
+    }
+
 	void Update () {
         if (Input.GetMouseButtonDown(0)) {
-            
+            Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.tag == "Enemy") {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+                
         }
 	}
 }
