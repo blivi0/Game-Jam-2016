e5a46016be0d8484d10d31ce02b249b871b2c65d
David Amadeo <amadeo.k.david@gmail.com>
2016-11-19 20:50:03 -0800

    Test for VR

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using VRStandardAssets.Utils;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -9,16 +10,25 @@ public class PlayerController : MonoBehaviour {
         camera = transform.FindChild("Camera").GetComponent<Camera>();
     }
 
-	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)) {
-                if (hit.collider.tag == "Enemy") {
-                    Destroy(hit.collider.gameObject);
-                }
-            }
-                
-        }
-	}
+    void Start() {
+        camera.GetComponent<VRInput>().OnClick += Shoot;
+    }
+
+//	void Update () {
+//        if (Input.GetMouseButtonDown(0)) {
+//            Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+//            RaycastHit hit;
+//            if (Physics.Raycast(ray, out hit)) {
+//                if (hit.collider.tag == "Enemy") {
+//                    Destroy(hit.collider.gameObject);
+//                }
+//            }
+//                
+//        }
+//	}
+
+    private void Shoot() {
+        Debug.Log("Shoot");
+        Destroy(GameObject.Find("Ground"));
+    }
 }
